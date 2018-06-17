1e0409b1021a04272c29ee22e0bdccbc2c1f5807
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-17 15:40:44 +0530

    to print the details of Students

---
+++ src/com/java/collection/StudentInformation.java
@@ -0,0 +1,21 @@
+package com.java.collection;
+
+
+	import java.util.ArrayList;
+	import java.util.Arrays;
+	import java.util.Comparator;
+	import java.util.List;
+
+	public class StudentInformation {
+	public static void main(String args[]) {
+		
+		List<String> List=new ArrayList<String>();
+		List.add("Bhuvana");
+		List.add("Kowsi");
+		List.add("Gowri");
+		List.add("Bhuvana");
+		System.out.println(List.toString());
+	
+	}
+	}	
+	
