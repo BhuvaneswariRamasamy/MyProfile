e38e231403f3af396c4c833813b28103ea3fc2aa
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-19 13:00:25 +0530

    To Find String Code

---
+++ src/com/logic/StringCode.java
@@ -0,0 +1,47 @@
+package com.logic;
+
+public class StringCode {
+	public static int getLetterNo(char Letter) {
+		return (int) Letter - 64;
+	}
+
+	public static int difference(int a, int b) {
+		int d = a - b;
+		if (d < 0)
+			d = d * (-1);
+
+		return d;
+	}
+
+	public static void main(String args[]) {
+		String input = "WORLD WIDE WEB";
+		int count ;
+		int n1, n2;
+		int sum = 0;
+		char c1;
+		char c2;
+		int center;
+		int output = 0;
+		String[] words = input.toUpperCase().split("\\s");
+		String total ="";
+		for (String w : words) {
+			count=0;
+			center = w.length() / 2;
+			sum = 0;
+			while (count < center) {
+				c1 = w.charAt(count);
+				c2 = w.charAt(w.length() - count - 1);
+				count++;
+				n1 = getLetterNo(c1);
+				n2 = getLetterNo(c2);
+				sum = sum + difference(n1, n2);
+			}
+			if (w.length() % 2 != 0)
+				sum = sum + getLetterNo(w.charAt(center));
+			total = total + sum;
+		}
+		output = Integer.valueOf(total);
+		System.out.println(output);
+
+	}
+}
