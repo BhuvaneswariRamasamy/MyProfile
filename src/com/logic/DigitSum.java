391a913c56cd8d119eea6c75e0c80a9c0e2ad5fd
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-24 16:22:28 +0530

    Program To find DigitSum of all Given Numbers

---
+++ src/com/logic/DigitSum.java
@@ -0,0 +1,31 @@
+package com.logic;
+
+public class DigitSum {
+public static void main(String args[]) {
+int input1=567, n,sum1=0,sum2=0,sum3=0;
+int count=0;
+if(input1<0) {
+	input1=Math.abs(input1);
+	count++;
+}
+ while(input1>0) {
+	n=input1%10;
+	sum1=sum1+n;
+	input1=input1/10;
+ }
+ while(sum1>0) {
+	n=sum1%10;
+	sum2=sum2+n;
+	sum1=sum1/10;
+ }
+ while(sum2>0) {
+	n=sum2%10;
+	sum3=sum3+n;
+	sum2=sum2/10;
+ }
+ if(count==1) {
+	sum3=-sum3;
+ }
+System.out.println(sum3);
+}
+}
