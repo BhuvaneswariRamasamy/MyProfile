1361e8831362fdc4ae115713f1cd8146afcad5cb
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-07-06 11:58:03 +0530

    Program to find the length of palindrome and the whether word can make a palindrome or not

---
+++ src/com/logic/Example.java
@@ -0,0 +1,13 @@
+package com.logic;
+
+import java.util.Scanner;
+
+public class Example {
+
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the word:");
+		char s=sc.next().charAt(0);
+		System.out.println(s);
+	}
+}
