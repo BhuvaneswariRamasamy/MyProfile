c5a06e07ea0b0de38a721ce7501ac0a1fd5a9f00
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-08-17 14:15:33 +0530

    Program to Print The Initial of First Letter

---
+++ src/com/logic/Initial.java
@@ -0,0 +1,15 @@
+package com.logic;
+
+public class Initial {
+public static void main(String[] args) {
+	String name="bhuvana";
+	char[] ch=name.toCharArray();
+//	String d=ch[0].toUpperCase();
+	for(int i=0;i<ch.length;i++) {
+	
+	}
+	char cc=ch[0];
+	String str=String.valueOf(cc).toUpperCase();
+	System.out.println("First Letter:"+str);
+	}
+}
