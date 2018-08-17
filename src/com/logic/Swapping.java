ca1752ad0820bb1146f7a39a70514eb325ed172a
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-08-17 16:38:01 +0530

    Simple program on Swapping

---
+++ src/com/logic/Swapping.java
@@ -0,0 +1,15 @@
+package com.logic;
+
+public class Swapping {
+public static void main(String[] args) {
+	int t=45;
+	int f=34;
+	int temp;
+	temp=t;
+	t=f;
+	f=temp;
+	System.out.println(t);
+	System.out.println(f);
+	System.out.println(temp);
+}
+}
