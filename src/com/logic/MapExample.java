75e64d0769505dfee983598cd66ac30503e12318
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-08-17 16:36:24 +0530

    Simple program on Map

---
+++ src/com/logic/MapExample.java
@@ -0,0 +1,16 @@
+package com.logic;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+public static void main(String[] args) {
+	Map<String,Integer> m=new HashMap<String,Integer>();
+	m.put("a", 10);
+	m.put("b", 12);
+	m.put("v", 34);
+	System.out.println(m);
+	
+}
+}
