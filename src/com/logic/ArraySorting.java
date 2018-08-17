bc82e1fa9c58f3a2da897dbf406ba190aba6293d
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-08-17 16:28:54 +0530

    Simple Program to Sorting an Array

---
+++ src/com/logic/ArraySorting.java
@@ -0,0 +1,13 @@
+package com.logic;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySorting {
+public static void main(String[] args) {
+	int a[]= {3,5,7,56,12,87};
+	Arrays.sort(a);
+	System.out.println(Arrays.toString(a));
+
+}
+}
